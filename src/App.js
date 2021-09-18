0a9d293cdbe6e0c1e29ab58b20c2561781aef858
Arif870 <arifuzzaman959@gmail.com>
2021-09-18 23:59:33 +0600

    challenge 9,10 uploaded

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <Blog heading="Padma nodir maji" author="jahir rayhan" />
-      <Blog heading="Mackbeth" author="sexpiar" />
-      <Blog heading="Miracle morning" author="will smith" />
+      <Mobile />
     </div>
   );
 }
 
-function Blog(props) {
+function Mobile() {
+  const [state, setstate] = useState(100);
+  let batteryHandalar = () => {
+    let newState = state - 10;
+
+    if (newState <= 0) {
+      newState = 0;
+    }
+    setstate(newState);
+  };
   return (
     <div className="blog">
-      <h1>{props.heading}</h1>
-      <p>Author : {props.author}</p>
+      <p>Battery Charge : {state}%</p>
+      <button className="btnDown" onClick={batteryHandalar}>
+        Baterry Down
+      </button>
     </div>
   );
 }
+
+// to do api call
+
 export default App;
