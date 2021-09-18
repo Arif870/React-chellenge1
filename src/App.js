541d6051edc1a4169234e5f66de9ccc3abd6a2d4
Arif870 <arifuzzaman959@gmail.com>
2021-09-18 23:25:59 +0600

    challenge 4 uploaded

--- src/App.js
+++ src/App.js
@@ -8,10 +8,17 @@ function App() {
   );
 }
 
+let headingStyle = {
+  fontSize: "40px",
+  fontWeight: 700,
+  color: "skyBlue",
+};
+
 function Blog() {
   return (
     <div>
       <article className="blog">
+        <h1 style={headingStyle}>This is Heading !!</h1>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem rerum
         quae obcaecati eius maxime laboriosam id consequuntur consequatur ipsam
         et.
