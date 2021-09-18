8faca9270dac820420037406784a58c227c8e164
Arif870 <arifuzzaman959@gmail.com>
2021-09-18 23:37:08 +0600

    challenge 6,7 uploaded

--- src/App.js
+++ src/App.js
@@ -3,38 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Blog />
+      <Blog heading="Padma nodir maji" author="jahir rayhan" />
+      <Blog heading="Mackbeth" author="sexpiar" />
+      <Blog heading="Miracle morning" author="will smith" />
     </div>
   );
 }
 
-let headingStyle = {
-  fontSize: "40px",
-  fontWeight: 700,
-  color: "skyBlue",
-};
-
-function Blog() {
+function Blog(props) {
   return (
-    <div>
-      <article className="blog">
-        <h1 style={headingStyle}>This is Heading !!</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem rerum
-        quae obcaecati eius maxime laboriosam id consequuntur consequatur ipsam
-        et.
-        <p
-          style={{
-            textAlign: "center",
-            backgroundColor: "green",
-            padding: "20px",
-            borderRadius: "10px",
-            fontSize: "24px",
-            fontWeight: "300",
-          }}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, animi?
-        </p>
-      </article>
+    <div className="blog">
+      <h1>{props.heading}</h1>
+      <p>Author : {props.author}</p>
     </div>
   );
 }
